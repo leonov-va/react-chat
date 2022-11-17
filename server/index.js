c1f40c3ff2a698bb158a0609ef1faad6f9967d2a
Vladislav Leonov <leonov.xi@gmail.com>
2022-11-18 01:51:01 +0300

    fixes template, init server

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const config = require("./config/app");
+
+const app = express();
+
+app.get("/home", (req, res) => {
+  return res.send("Home screen");
+});
+
+app.get("/login", (req, res) => {
+  return res.send("Login screen!!");
+});
+
+const port = config.appPort;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
